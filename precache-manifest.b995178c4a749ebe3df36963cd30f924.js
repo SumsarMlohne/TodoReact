567f8416175c2b4fc65d342539123f53e8af6956
SumsarMlohne <rasmus.enholm@hotmail.com>
2020-10-23 17:32:21 +0300

    Updates

---
+++ precache-manifest.b995178c4a749ebe3df36963cd30f924.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2df1395979c0ad8b67c53f66f829ffe4",
+    "url": "/TodoReact/index.html"
+  },
+  {
+    "revision": "96e02782aff18942b5bd",
+    "url": "/TodoReact/static/css/main.220fcbac.chunk.css"
+  },
+  {
+    "revision": "316ba29e3ba630ed3938",
+    "url": "/TodoReact/static/js/2.15054dbf.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/TodoReact/static/js/2.15054dbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96e02782aff18942b5bd",
+    "url": "/TodoReact/static/js/main.02aefe76.chunk.js"
+  },
+  {
+    "revision": "a29b0a09262e2a412062",
+    "url": "/TodoReact/static/js/runtime-main.37b0c47b.js"
+  }
+]);
